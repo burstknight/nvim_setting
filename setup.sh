87d52e8bd77f65b2156a6bbdf5ee5285d5c6be24
burstknight <burstknight@gamil.com>
2024-06-02 23:20:21 +0800

    chore: Add shell file for init
    - Target:
      This repo need add a shell file to automatically initialize the config
      for neovim.
    
    - Changed:
      Add the shell file `setup.sh`.

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+###############################################################################
+# Description: Initialize the setting for neovim.
+###############################################################################
+
+echo "Copy the directory 'nvim' into '~/.config' ..."
+cp -R ./nvim ~/.config/
+echo "Done!"
+
