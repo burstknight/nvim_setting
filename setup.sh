8ace116451c902561c73ccc4e23883fc66d30f13
burstknight <burstknight@gmail.com>
2024-06-05 11:47:50 +0800

    chore: Add docker file to test `setup.sh`
    - Target:
      I need use docker to test the shell file `setup.sh`, so I should add
      a docker file.
    
    - Changed:
      Add the docker file `Dockerfile.ubuntu.test` to test the shell file
      `setup.sh` on `ubuntu:22.04`.

--- setup.sh
+++ setup.sh
@@ -3,10 +3,16 @@
 # Description: Initialize the setting for neovim.
 ###############################################################################
 
+which sudo > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	alias sudo=': && '
+fi
+
 sudo apt-get update
 sudo apt-get install -y git curl
 
-echo "Copy the directory 'nvim' into '~/.config' ..."
-cp -R ./nvim ~/.config/
+echo "Copy the directory 'nvim' into '$HOME/.config' ..."
+mkdir -p $HOME/.config/nvim
+cp -r ./nvim/* $HOME/.config/nvim/
 echo "Done!"
 
