5bffdb6ef060bd125d9796c5fef7532b36fa7b48
burstknight <burstknight@gmail.com>
2024-06-05 10:26:04 +0800

    chore: Update `setup.sh` for requirements
    - Target:
      The shell file `setup.sh` need be modified to install requirements.
    
    - Changed:
      Update the shell file `setup.sh` for the requirements.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 # Description: Initialize the setting for neovim.
 ###############################################################################
 
+sudo apt-get update
+sudo apt-get install -y git curl
+
 echo "Copy the directory 'nvim' into '~/.config' ..."
 cp -R ./nvim ~/.config/
 echo "Done!"
