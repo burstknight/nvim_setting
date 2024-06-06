e4c223d7803c535242b13ccc3ab00de2e663b612
burstknight <burstknight@gamil.com>
2024-06-06 22:46:05 +0800

    feat: Add plugin `vim-airline`
    - Target:
      I need use the plugin `vim-airline` to show cool status bar.
    
    - Changed:
      Add the plugin `vim-airline` to show cool status bar.

--- nvim/lua/plugin_manager.lua
+++ nvim/lua/plugin_manager.lua
@@ -10,4 +10,8 @@ Plug 'preservim/nerdtree'
 -- 這個外掛可以用來括號類符號的自動補齊
 Plug 'jiangmiao/auto-pairs'
 
+-- 使用這兩個外掛來顯示vim的狀態列
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+
 vim.call('plug#end')
