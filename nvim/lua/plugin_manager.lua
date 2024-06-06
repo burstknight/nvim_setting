708cb1b9158b9f00138157caf2d9dd075b3c6a85
burstknight <burstknight@gamil.com>
2024-06-06 22:55:16 +0800

    feat: Add plugin `nerdtree-git-plugin`
    - Target:
      I need the plugin `nerdtree-git-plugin` to show the status of each
      file on `nerdtree`.
    
    - Changed:
      Add the plugin `nerdtree-git-plugin`.

--- nvim/lua/plugin_manager.lua
+++ nvim/lua/plugin_manager.lua
@@ -7,6 +7,9 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
 -- 這個外掛可以當檔案總管來瀏覽一個專案目錄下的檔案
 Plug 'preservim/nerdtree'
 
+-- 可以配合 nerdtree 顯示每個檔案git的狀態
+Plug 'Xuyuanp/nerdtree-git-plugin'
+
 -- 這個外掛可以用來括號類符號的自動補齊
 Plug 'jiangmiao/auto-pairs'
 
