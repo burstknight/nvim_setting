17d72012c6290fd8424b6a62a6397ec814a93fb2
burstknight <burstknight@gamil.com>
2024-06-07 22:35:58 +0800

    feat: Update `settings.lua` to show location for cursor
    - Target:
      I need show the current location of the cursor.
    
    - Changed:
      Update the file `settings.lua` to show the current location of the
      cursor.

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,3 +1,5 @@
+local vim = vim
+
 vim.o.nu = true 				-- 顯示行數
 vim.o.tabstop = 4 				-- 設定按下 tab 鍵時產生4格空白字元
 vim.o.shiftwidth = 4 			-- 設定自動縮排對齊寬度為4格
@@ -6,3 +8,5 @@ vim.o.encoding = "utf-8" 		-- 設定使用 utf-8 來顯示讀取的檔案
 vim.o.fileencoding = "utf-8" 	-- 使用 utf-8 來儲存檔案
 vim.o.linebreak = true 			-- 如果一行的文字過長，就將其拆行顯示在下一行
 vim.o.showbreak = ">>" 			-- 設定拆行的接續符號
+vim.o.cursorline = true 		-- 凸顯出游標所在的行
+vim.o.cursorcolumn = true 		-- 凸顯出游標所在的某一行的位置
