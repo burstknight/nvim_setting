89c0a55524112648d1cfa8a249717097180af5e7
burstknight <burstknight@gamil.com>
2024-06-09 22:35:32 +0800

    feat: Use `lazy.nvim` as plugin manager
    - Target:
      I need use `lazy.nvim` to manage all plugins for neovim.
    
    - Changed:
      Use `lazy.nvim` to manage all plugins.

--- nvim/lua/plugin_manager.lua
+++ nvim/lua/plugin_manager.lua
@@ -1,32 +1,40 @@
-local vim = vim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-local Plug = vim.fn['plug#']
+require("lazy").setup({
+	-- 這個外掛可以當檔案總管來瀏覽一個專案目錄下的檔案
+	{'preservim/nerdtree'},
 
-vim.call('plug#begin', '~/.config/nvim/plugged')
+	-- 可以配合 nerdtree 顯示每個檔案git的狀態
+	{'Xuyuanp/nerdtree-git-plugin'},
 
--- 這個外掛可以當檔案總管來瀏覽一個專案目錄下的檔案
-Plug 'preservim/nerdtree'
+	-- 這個外掛可以用來括號類符號的自動補齊
+	{'jiangmiao/auto-pairs'},
+	--
+	-- 使用這兩個外掛來顯示vim的狀態列
+	{'vim-airline/vim-airline'},
+	{'vim-airline/vim-airline-themes'},
 
--- 可以配合 nerdtree 顯示每個檔案git的狀態
-Plug 'Xuyuanp/nerdtree-git-plugin'
+	-- 可以和 git 搭配及時顯示當前檔案的修改狀態
+	{'airblade/vim-gitgutter'},
 
--- 這個外掛可以用來括號類符號的自動補齊
-Plug 'jiangmiao/auto-pairs'
+	-- 這個外掛可以產生Doxygen格式的註解
+	{'vim-scripts/DoxygenToolkit.vim'},
 
--- 使用這兩個外掛來顯示vim的狀態列
-Plug 'vim-airline/vim-airline'
-Plug 'vim-airline/vim-airline-themes'
+	-- 主題外掛
+	{'tomasiser/vim-code-dark'},
 
--- 可以和 git 搭配及時顯示當前檔案的修改狀態
-Plug 'airblade/vim-gitgutter'
+	-- 可以使用多個不同顏色來顯示巢狀式括號
+	{'frazrepo/vim-rainbow'},
+})
 
--- 這個外掛可以產生Doxygen格式的註解
-Plug 'vim-scripts/DoxygenToolkit.vim'
-
--- 主題外掛
-Plug 'tomasiser/vim-code-dark'
-
--- 可以使用多個不同顏色來顯示巢狀式括號
-Plug 'frazrepo/vim-rainbow'
-
-vim.call('plug#end')
