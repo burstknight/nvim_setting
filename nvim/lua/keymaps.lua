686414b52ec560a85a9693713a629d82f7966aeb
burstknight <burstknight@gmail.com>
2024-06-11 10:15:31 +0800

    feat: Add plugin `telescope`
    - Target:
      I need the plugin `telescope` to fuzzy find files.
    
    - Changed:
      Add the plugin `telescope`.

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -8,3 +8,9 @@ vim.api.nvim_set_keymap('n', '<F2>', ':NERDTreeToggle<CR>', keymap_opts)
 -- 設定外掛 DoxygenToolkit.vim 可以使用 <F8> 插入函數的說明註解
 vim.api.nvim_set_keymap('n', "<F8>", ":Dox<CR>", keymap_opts)
 
+-- 設定外掛 telescope 使用 Ctrl + p 搜尋檔案
+vim.api.nvim_set_keymap("n", "<C-p>", ":Telescope find_files<CR>", keymap_opts)
+
+-- 設定外掛telescope 使用 Ctrl + f 全域搜尋
+vim.api.nvim_set_keymap("n", "<C-f>", ":Telescope live_grep<CR>", keymap_opts)
+
