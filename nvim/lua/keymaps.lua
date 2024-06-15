578a4644b35781e9eba91ad60b42e0ae7ae8fa6e
burstknight <burstknight@gamil.com>
2024-06-15 14:53:37 +0800

    feat: Update shortcut use `lspsaga` for hover
    - Target:
      I need update the shortcut to use the plugin `lspsaga` to show
      documents.
    
    - Changed:
      Update the shortcut to use the plugin `lspsaga` for hover.

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -26,7 +26,7 @@ pluginKeys.lspKeyBinding = function(mapbuf)
 	mapbuf("n", "<leader>rn", ":lua vim.lsp.buf.rename()<CR>", opts)
 	mapbuf("n", "<leader>ca", ":lua vim.lsp.buf.code_action()<CR>", opts)
 	mapbuf("n", "gd", ":lua vim.lsp.buf.definition()<CR>", opts)
-	mapbuf("n", "<F9>", ":lua vim.lsp.buf.hover()<CR>", opts)
+	mapbuf("n", "<leader>gh", "<cmd>Lspsaga hover_doc<CR>", opts)
 end
 
 pluginKeys.cmpKeys = function(cmp)
