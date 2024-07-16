95e5df0808f5a8874cb0649cf92518ad1e800b4b
burstknight <burstknight@gmail.com>
2024-07-16 17:02:17 +0800

    feat: Add `init.py`
    - Target:
      I want to use python code as a tool to set the setting for neovim.
    
    - Changed:
      Add the python file `init.py`.

---
+++ init.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from os import system
+from os.path import exists, join
+from shutil import copytree, rmtree
+from pathlib import Path
+
+
+def installRequirements():
+	"""
+	Description:
+	=======================================================
+	Install all requirements for neovim.
+	"""
+	system("sudo apt-get update")
+	system("sudo apt-get install -y git curl ripgrep fd-find clangd python3 python3-pip python3-venv")
+# End of installRequirements
+
+def copySettingFiles():
+    sTargetPath = join(Path.home(), ".config/nvim")
+    if True == exists(sTargetPath):
+        rmtree(sTargetPath)
+    # End of if-condition
+
+    print("Copy the directory 'nvim' into '$HOME/.config' ...")
+    copytree("./nvim/", sTargetPath)
+    print("Done!")
+# End of copySettingFiles
+
+
+def main():
+    installRequirements()
+    copySettingFiles()
+# End of main
+
+if "__main__" == __name__:
+	main()
+# End of if-condition
