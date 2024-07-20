6b4c4e44c11507d42d8946374983d1fe02c91e50
burstknight <burstknight@gmail.com>
2024-07-20 23:15:23 +0800

    fix: Not found `provider.python` on windows
    - Problem:
      Neovim issued the error about `not found provider.python` on windows.
    
    - Reason:
      The file `settings.lua` didn't set the python path for `provider.python`
      on windows.
    
    - Solution:
      Update the file `settings.lua` to fix this bug.

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -7,6 +7,21 @@ local vim = vim
  -- 開啟支援 24 位元彩色
  vim.opt.termguicolors = true
 
+ if vim.fn.exists("g:os") == 0 then
+ 	local is_windows = vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 or vim.fn.has("win16") == 1
+	if is_windows then
+		vim.g.os = "Windows"
+	else
+		local uname_output = vim.fn.system("uname")
+		vim.g.os = string.gsub(uname_output, "\n", " ")
+	end
+ end
+
+
+ if "Windows" == vim.g.os then
+ 	vim.cmd("let g:python3_host_prog=\"~/.pyenv/pyenv-win/shims/python\"")
+ end
+
 vim.o.nu = true 				-- 顯示行數
 vim.o.tabstop = 4 				-- 設定按下 tab 鍵時產生4格空白字元
 vim.o.shiftwidth = 4 			-- 設定自動縮排對齊寬度為4格
