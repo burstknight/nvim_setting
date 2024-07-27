d8660571bc6ec8e89994744f3171f7e6df2361e2
burstknight <burstknigh@gmail.com>
2024-07-27 15:21:25 +0800

    chore: Update `init.py` for `fd` on windwos
    - Target:
      The python file `init.py` need be changed to install the tool `fd` for
      the plugin `telescope.nvim` on windows.
    
    - Changed
      Update the python file `init.py` to install the tool `fd` on windows.

--- init.py
+++ init.py
@@ -17,7 +17,7 @@ def installRequirements():
 		system("sudo apt-get update")
 		system("sudo apt-get install -y git curl ripgrep fd-find clangd python3 python3-pip python3-venv")
 	else:
-		system("choco install --yes luarocks tree-sitter nerd-fonts-hack nodejs pwsh unzip yarn ripgrep 7zip.install")
+		system("choco install --yes luarocks tree-sitter nerd-fonts-hack nodejs pwsh unzip yarn ripgrep 7zip.install fd")
 	# End of if-condition
 # End of installRequirements
 
