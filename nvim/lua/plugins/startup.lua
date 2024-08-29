3394699027817b3f66c907edccd6600b61d7ce3c
burstknight <burstknight@gmail.com>
2024-08-29 16:31:02 +0800

    feat: Update `startup.lua` for keymaps
    - Target:
      The setting file `startup.lua` need be changed to change the keymaps
      for the plugin `startup.nvim`.
    
    - Changed
      Update the setting file `startup.nvim` to change the keymaps for the
      plugin `startup.nvim`.

--- nvim/lua/plugins/startup.lua
+++ nvim/lua/plugins/startup.lua
@@ -40,7 +40,24 @@ startup.setup({
 		default_color = "",
 		oldfiles_amount = 0,
 	},
+	body = {
+		type = "mapping",
+		oldfiles_directory = false,
+		align = "center",
+		fold_section = false,
+		title = "Basic Commands",
+		margin = 0,
+		content = {
+			{"  Find file", "Telescope find_files", "<C-p>"},
+			{"  File manager", "NERDTreeToggle", "<F2>"},
+			{"  Recent", "Telescope oldfiles", "<leader>fr"},
+		},
+		highlight = "String",
+		default_color = "",
+		oldfiles_amount = 0,
+	},
 	parts = {
 		"header",
+		"body",
 	},
 })
