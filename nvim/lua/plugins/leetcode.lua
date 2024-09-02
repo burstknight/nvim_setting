a2138609500fa3e1a22d8c13e48183a15089e2c7
burstknight <burstknight@gmail.com>
2024-09-02 15:21:11 +0800

    feat: Update setting for `leetcode.nvim`
    - Target:
      The setting file `leetcode.lua` need be changed for the plugin
      `leetcode.nvim`.
    
    - Changed
      Update the setting for `leetcode.nvim`.

--- nvim/lua/plugins/leetcode.lua
+++ nvim/lua/plugins/leetcode.lua
@@ -5,4 +5,74 @@ if not status then
 end
 
 leetcode.setup({
+	arg = "leetcode",
+	lang = "cpp",
+	storage = {
+		home = vim.fn.stdpath("data") .. "/leetcode",
+		cache = vim.fn.stdpath("cache") .. "/leetcode",
+	},
+	plugins = {
+		non_standalone = true,
+	},
+	logging = true,
+	cache = {
+		update_interval = 60*60*24*7,
+	},
+	console = {
+		open_on_runcode = true,
+		dir = "row",
+		size = {
+			width = "90%",
+			height = "75%",
+		},
+		result = {
+			size = "60%",
+		},
+		testcase = {
+			virt_text = true,
+			size = "40%",
+		},
+	},
+
+	description = {
+		position = "left",
+		width = "40%",
+		show_stats = true,
+	},
+
+	hooks = {
+		["enter"] = {},
+		["question_enter"] = {},
+		["leave"] = {},
+	},
+
+	injector = {
+		["cpp"] = {
+			before = {
+				"#include <bits/stdc++.h>",
+				"using namespace std;",
+			},
+			after = "int main() {}",
+		},
+		["c"] = {
+			before = {
+				"#include <stdio.h>",
+				"#include <stdlib.h>",
+				"#include <stdbool.h>",
+				"#include <stddef.h>",
+			},
+			after = "int main() {}",
+		},
+	},
+
+	keys = {
+		toggle = {"q"},
+		confirm = {"<CR>"},
+		reset_testcases = "r",
+		use_testcase = "U",
+		focus_testcase = "H",
+		focus_result = "L",
+	},
+	theme = {},
+	image_support = false,
 })
