92bf44e9dfd74e8c305d5d3d5066708f950bc396
burstknight <burstknight@gmail.com>
2024-10-19 16:28:39 +0800

    feat: Change keymaps for `chadtree`
    - Target:
      Some keymaps of the plugin `chadtree` should be changed for my habit.
    
    - Changed
      Change some keymaps for the plugin `chadtree`.

--- nvim/lua/plugins/chadtree.lua
+++ nvim/lua/plugins/chadtree.lua
@@ -1,5 +1,18 @@
 local nvim_set_var = vim.api.nvim_set_var
 
-local chadtree_settings = {}
+local chadtree_settings = {
+	keymap = {
+		change_dir = {"W"},
+		change_focus = {},
+		change_focus_up = {"U"},
+		collapse = {"c"},
+		stat = {"L"},
+		h_split = {"i"},
+		v_split = {"I"},
+		toggle_version_control = {},
+		toggle_follow = {},
+		toggle_follow_links = {},
+	},
+}
 
 nvim_set_var("chadtree_settings", chadtree_settings)
