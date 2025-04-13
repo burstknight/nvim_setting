417d8391f7061b505983aeebeefa6c491bfa9192
burstknight <burstknight@gmail.com>
2025-04-13 14:59:35 +0800

    feat: Dap support `C#`
    - Target:
      I need add a setting file to use neovim to debug `C#` code.
    
    - Changed
      Add the setting file `csharp.lua` to use neovim to debug `C#` code.

---
+++ nvim/lua/dap/csharp.lua
@@ -0,0 +1,17 @@
+local dap = require("dap")
+dap.adapters.coreclr = {
+	type = "executable",
+	command = vim.fn.stdpath("data") .. "/mason/packages/netcoredbg/netcoredbg",
+	args = { "--interpreter=vscode" },
+}
+
+dap.configurations.cs = {
+	{
+		type = "coreclr",
+		name = "launch - netcoredbg",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+		end,
+	},
+}
