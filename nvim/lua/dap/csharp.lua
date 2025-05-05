3acad5945eb7995abe9641ea7ab72ae0cd9e40cb
burstknight <burstknight@gmail.com>
2025-05-05 11:44:53 +0800

    feat: Update `C#` dap setting for windows
    - Target:
      The `C#` dap setting need be changed for windows.
    
    - Changed
      Update the file `csharp.lua` to change the `C#` dap setting for
      windows.

--- nvim/lua/dap/csharp.lua
+++ nvim/lua/dap/csharp.lua
@@ -1,8 +1,21 @@
 local dap = require("dap")
+dap.set_log_level("TRACE")
+
+local function getDebugBinPath()
+	local get_platform = require("utils.init").get_platform
+	if "Windows" == get_platform() then
+		return vim.fn.expand("~/scoop/apps/netcoredbg/current/netcoredbg.exe")
+	else
+		return vim.fn.expand(vim.fn.stdpath("data") .. "/mason/packages/netcoredbg/netcoredbg")
+	end
+end
+
 dap.adapters.coreclr = {
 	type = "executable",
-	command = vim.fn.stdpath("data") .. "/mason/packages/netcoredbg/netcoredbg",
-	args = { "--interpreter=vscode" },
+	command = getDebugBinPath(),
+	args = {
+		"--interpreter=vscode",
+	},
 }
 
 dap.configurations.cs = {
@@ -11,7 +24,19 @@ dap.configurations.cs = {
 		name = "launch - netcoredbg",
 		request = "launch",
 		program = function()
-			return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+			local build_output = vim.fn.systemlist("dotnet build -c Debug")
+			local dotnet_last_dll_path = ""
+			for _, line in pairs(build_output) do
+				vim.notify("Build file path: `" .. line .. "`")
+				if line:match(".dll") then
+					dotnet_last_dll_path = line
+					break
+				end
+			end
+			local _, end_index = string.find(dotnet_last_dll_path, "-> ")
+			dotnet_last_dll_path = string.sub(dotnet_last_dll_path, end_index)
+			vim.notify("Dll path: `" .. dotnet_last_dll_path .. "`")
+			return dotnet_last_dll_path
 		end,
-	},
+	}
 }
