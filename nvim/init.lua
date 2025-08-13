26b5e5cbade908dbc48d81bdb90b8f96422e6d39
burstknight <burstknight@gmail.com>
2025-08-13 16:08:49 +0800

    feat: Add `init.lua` into `nvim/lua/Plugins`
    - Target:
      The folder `nvim/lua/Plugins` need be added a lua file to easily
      include all settings for the plugins.
    
    - Changed
      Add the file `init.lua` into the folder `nvim/lua/Plugins`.

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,25 +10,8 @@ require("keymaps")
 -- 載入主題設定
 require("theme")
 
--- 載入所有外掛的設定檔
-local plugins = {
-	"Plugins.nvim-notify",
-	"Plugins.chadtree",
-	"Plugins.vim-gitgutter",
-	"Plugins.vim-rainbow",
-	"Plugins.ibl",
-	"Plugins.gitignore",
-	"Plugins.startup",
-	"Plugins.leetcode",
-	"Plugins.lualine"
-}
-
-for i = 1, #plugins do
-	local isOk, msg = pcall(require, plugins[i])
-	if false == isOk then
-		print("Failed to load the plugin setting: " .. plugins[i] .. "\nReason: " .. msg)
-	end
-end
+-- Load all settings for the plugins.
+require("Plugins.init")
 
 -- Include settings for lsp.
 require("Lsp.init")
